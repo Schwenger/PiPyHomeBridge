12b4fb4be1199a9efe05cb1f59735adc40cef43c
Maximilian Schwenger <schwengermaximilian@gmail.com>
2023-02-03 23:02:39 +0100

    Add logging to file.

--- src/log.py
+++ src/log.py
@@ -4,23 +4,34 @@ INFO = False
 ALERT = True
 TRACE = False
 
+WRITE_TO_FILE = True
+
+PATH = '/home/pi/log'
+
 def trace(name: str, fun: str, cls: str = ""):
     "Traces function calls."
     if not TRACE:
         return
     if cls != "":
         cls = "(" + cls + ") "
-    print(name + " " + cls + fun)
+    _put(name + " " + cls + fun)
 
 def info(msg: str):
     "Prints an info"
     if not INFO:
         return
-    print(msg)
+    _put(msg)
 
 def alert(msg: str):
     "Prints a warning"
     if not ALERT:
         return
-    print("****WARNING****")
-    print(msg)
+    _put("****WARNING****")
+    _put(msg)
+
+def _put(msg: str):
+    if WRITE_TO_FILE:
+        with open(PATH, "a", encoding="utf-8") as file:
+            file.write(msg)
+    else:
+        print(msg)

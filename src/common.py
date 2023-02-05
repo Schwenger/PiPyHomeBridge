9988fd4b4d9c4b0adb93ce8114ab8d8d9432fc39
Maximilian Schwenger <schwengermaximilian@gmail.com>
2023-02-05 10:50:15 +0100

    More robust locating of config.

--- src/common.py
+++ src/common.py
@@ -1,6 +1,7 @@
 "A module for common functionality"
 
 import platform
+import os
 import yaml
 
 NODE_NAME = platform.node()
@@ -9,9 +10,13 @@ if 'Mairxbook' in NODE_NAME:
 else:
     CLIENT_NAME = 'Pi'
 
-with open("config.yml", "r", encoding="utf-8") as stream:
+
+base_path = os.path.dirname(os.path.realpath(__file__))
+cfg_path = os.path.join(base_path, '..', 'config.yml')
+with open(cfg_path, "r", encoding="utf-8") as stream:
     try:
         config = yaml.safe_load(stream)
+        print(config)
     except yaml.YAMLError as exc:
         print("Failed to load config file config.yml.")
         raise exc
